df0a2151aff75a38a35dfc805a64a85f5d9ba061
Janny K <janny@vipicash.com>
2019-07-01 15:55:07 +0200

    initial commit

---
+++ src/main/java/no/fremtind/insurance/insuranceservice/InsuranceServiceApplication.java
@@ -0,0 +1,13 @@
+package no.fremtind.insurance.insuranceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceServiceApplication.class, args);
+    }
+
+}
