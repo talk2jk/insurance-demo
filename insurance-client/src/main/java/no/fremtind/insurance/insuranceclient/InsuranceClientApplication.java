1323742264633e8b4288179a050f02ab286a1162
Janny K <janny@vipicash.com>
2019-07-01 16:21:54 +0200

    Added other components/applications as maven module.

---
+++ insurance-client/src/main/java/no/fremtind/insurance/insuranceclient/InsuranceClientApplication.java
@@ -0,0 +1,13 @@
+package no.fremtind.insurance.insuranceclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceClientApplication.class, args);
+    }
+
+}
---
+++ integration-layer/src/main/java/no/fremtind/insurance/integrationlayer/IntegrationLayerApplication.java
@@ -0,0 +1,12 @@
+package no.fremtind.insurance.integrationlayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationLayerApplication.class, args);
+	}
+}
