e60d0831499cac96f29d03edd7ce417a4406b2d7
Janny K <janny@vipicash.com>
2019-07-02 04:20:40 +0200

    added integration config in xml

---
+++ integration-layer/src/main/java/no/fremtind/insurance/integrationlayer/dto/InsuranceOrderResponse.java
@@ -0,0 +1,9 @@
+package no.fremtind.insurance.integrationlayer.dto;
+
+import java.io.Serializable;
+
+public class InsuranceOrderResponse implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    // TODO
+}
